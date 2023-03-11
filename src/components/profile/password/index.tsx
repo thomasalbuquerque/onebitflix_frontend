a53fa135299d59df4d77722926b047f671d55350
Thomas Albuquerque <tealbthomas@gmail.com>
2023-03-11 14:19:23 -0300

    Conexão do backend relacionado ao update de senha. Criação de um método e também de toda a parte de conexão com o front - Conexão backend update da senha

--- src/components/profile/password/index.tsx
+++ src/components/profile/password/index.tsx
@@ -1,10 +1,73 @@
+import ToastComponent from "@/components/common/toast";
+import profileService from "@/services/profileService";
+import { FormEvent, useEffect, useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import styles from "../../../styles/profile.module.scss";
 
 const PasswordForm = function () {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [color, setColor] = useState("");
+  const [toastIsOpen, setToastIsOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    profileService.fetchCurrent().then((data) => {
+      setCurrentPassword(data.currentPassword);
+      setNewPassword(data.newPassword);
+    });
+  }, []);
+
+  const handlePasswordUpadate = async function (event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    if (newPassword != confirmPassword) {
+      setToastIsOpen(true);
+      setErrorMessage("Senha e confirmação de senha diferentes!");
+      setColor("bg-danger");
+      setTimeout(() => setToastIsOpen(false), 1000 * 3);
+  
+      return;
+    }
+
+    if (currentPassword === newPassword) {
+      setToastIsOpen(true);
+      setErrorMessage("Não coloque a nova senha igual a senha antiga!");
+      setColor("bg-danger");
+      setTimeout(() => setToastIsOpen(false), 1000 * 3);
+  
+      return;
+    }
+
+    const res = await profileService.passwordUpdate({
+      currentPassword,
+      newPassword,
+    });
+
+    if (res === 204) {
+      setToastIsOpen(true);
+      setErrorMessage("Senha alterada com sucesso!");
+      setColor("bg-success");
+      setTimeout(() => setToastIsOpen(false), 1000 * 3);
+
+      setCurrentPassword("");
+      setNewPassword("");
+      setConfirmPassword("");
+    }
+
+    if (res === 400) {
+      setToastIsOpen(true);
+      setErrorMessage("Senha atual incorreta!");
+      setColor("bg-danger");
+      setTimeout(() => setToastIsOpen(false), 1000 * 3);
+    }
+  };
+
   return (
     <>
-      <Form className={styles.form}>
+      <Form className={styles.form} onSubmit={handlePasswordUpadate}>
         <div className={styles.inputNormalDiv}>
           <FormGroup>
             <Label className={styles.label} for="currentPassword">
@@ -18,6 +81,10 @@ const PasswordForm = function () {
               required
               maxLength={12}
               className={styles.input}
+              value={currentPassword}
+              onChange={(event) => {
+                setCurrentPassword(event.currentTarget.value);
+              }}
             />
           </FormGroup>
         </div>
@@ -32,7 +99,11 @@ const PasswordForm = function () {
               id="newPassword"
               placeholder="******"
               required
-              className={styles.inputFlex}
+              className={styles.inputFlex} 
+              value={newPassword}
+              onChange={(event) => {
+                setNewPassword(event.currentTarget.value);
+              }}
             />
           </FormGroup>
           <FormGroup>
@@ -46,13 +117,22 @@ const PasswordForm = function () {
               placeholder="******"
               required
               className={styles.inputFlex}
+              value={confirmPassword}
+              onChange={(event) => {
+                setConfirmPassword(event.currentTarget.value);
+              }}
             />
           </FormGroup>
         </div>
-        <Button className={styles.formBtn} outline>
+        <Button className={styles.formBtn} outline type="submit">
           Salvar Alterações
         </Button>
       </Form>
+      <ToastComponent
+        color={color}
+        isOpen={toastIsOpen}
+        message={errorMessage}
+      />
     </>
   );
 };
--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -8,42 +8,68 @@ interface UserParams {
   createdAt: string; //o createdAt nao e retornado pelo backend
 }
 
+interface PasswordParams {
+  currentPassword: string;
+  newPassword: string;
+}
+
 const profileService = {
   fetchCurrent: async () => {
     const token = sessionStorage.getItem("onebitflix-token");
-  
-    const res = await api.get("/users/current", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .catch((error) => {
-      console.log(error.response.data.message);
-  
-      return error.response;
-    });
-  
+
+    const res = await api
+      .get("/users/current", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+
+        return error.response;
+      });
+
     return res.data;
   },
 
   userUpdate: async (params: UserParams) => {
     const token = sessionStorage.getItem("onebitflix-token");
-  
-    const res = await api.put("/users/current", params, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    })
-    .catch((error) => {
-    if (error.response.status === 400 || error.response.status === 401) {
-      return error.response;
-    }
-  
-    return error;
-    });
-  
+
+    const res = await api
+      .put("/users/current", params, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        if (error.response.status === 400 || error.response.status === 401) {
+          return error.response;
+        }
+
+        return error;
+      });
+
+    return res.status;
+  },
+
+  passwordUpdate: async (params: PasswordParams) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api.put("/users/current/password", params, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        if (error.response.status === 400 || error.response.status === 401) {
+          return error.response;
+        }
+
+        return error;
+      });
+
     return res.status;
   },
 };
-    
+
 export default profileService;
